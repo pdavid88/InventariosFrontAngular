38853f80fbf6da039e342061a2f9377c6d981d25
pdavid88 <david.alg88@gmail.com>
2024-07-11 13:10:24 +0200

    InventariosFrontAngular 1.2 - Creado servicio producto y componente producto-lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,9 @@ import { ProductoListaComponent } from './producto-lista/producto-lista.componen
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/producto-lista/producto-lista.component.ts
+++ src/app/producto-lista/producto-lista.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html'
 })
 export class ProductoListaComponent {
+
+  productos: Producto[];
+
+  constructor(private productoServicio: ProductoService) { }
+
+  ngOnInit(): void {
+    // Cargamos todos los productos
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos() {
+    // Consumir los datos del observable (suscribirnos)
+    this.productoServicio.obtenerProductosLista().subscribe(
+      (datos => {
+        this.productos = datos;
+      })
+    );
+  }
 
 }

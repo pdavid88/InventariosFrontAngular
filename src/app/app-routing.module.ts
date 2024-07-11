dd4c5ca60d0d7792a0c622e89adc727d393f5cb1
pdavid88 <david.alg88@gmail.com>
2024-07-11 18:35:00 +0200

    InventariosFrontAngular 1.4 - Agregado rutas y modificado app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: 'productos', component: ProductoListaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
